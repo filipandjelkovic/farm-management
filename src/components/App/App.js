45b0f31e059d607127f0e7c44780259e6ff0543d
filipandjelkovic <filipandjelkovic@protonmail.com>
2020-03-10 22:47:30 +0100

    Init react app

---
+++ src/components/App/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './../../assets/logo.svg';
+import './../../styles/App.css';
+import * as AppController from './../../controllers/AppController';
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {AppController.getName()} <code>src/App.js</code> and save to reload.
+          </p>
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
